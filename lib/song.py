289f0906f4075d374be804c6ace3aa22bf8bd977
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-06-04 22:57:50 +0300

    feat: add class attributes

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,9 @@
 class Song:
-    pass
+    # Class attributes
+    count = 0  # Tracking total number of songs
+    genres = []  # List to store all genres (duplicates allowed initially)
+    artists = []  # List to store all artists (duplicates allowed initially)
+    genre_count = {}  # Dictionary to store genre histogram
+    artist_count = {}  # Dictionary to store artist histogram
+
+    
