7ac388f618333c03e77563c68a09a81b262803ca
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-06-05 15:02:56 +0300

    updated class Song to keep count of every song in each genre, each list

--- lib/song.py
+++ lib/song.py
@@ -1,8 +1,10 @@
 class Song:
     # Class attributes
     count = 0  # Tracking total number of songs
-    genres = []  # List to store all genres (duplicates allowed initially)
-    artists = []  # List to store all artists (duplicates allowed initially)
+    genres = []  # List to store all genres (with duplicates)
+    artists = []  # List to store all artists (with duplicates)
+    unique_genres = set()  # Store unique genres
+    unique_artists = set()  # Store unique artists
     genre_count = {}  # Dictionary to store genre histogram
     artist_count = {}  # Dictionary to store artist histogram
 
@@ -17,15 +19,13 @@ class Song:
         self._genre = genre
 
         # Add to class-level tracking
-        Song.genres.append(genre)  # Adds genre to genres list
-        Song.artists.append(artist)  # Adds artist to artists list
-
-        # Calls class methods to update counts and lists (unique)
-        self.add_song_to_count()
-        self.add_to_genres()
-        self.add_to_artists()
-        self.add_to_genre_count()
-        self.add_to_artist_count()
+        Song.count += 1
+        Song.genres.append(genre)  # Keep duplicates for counting
+        Song.artists.append(artist)  # Keep duplicates for counting
+        Song.unique_genres.add(genre)  # Track unique genres
+        Song.unique_artists.add(artist)  # Track unique artists
+        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
 
     # Property methods for validation
     @property
@@ -64,34 +64,34 @@ class Song:
             raise ValueError("Genre must be a non-empty string")
         self._genre = value
 
-    # Class methods
-    @classmethod
-    def add_song_to_count(cls):
-        """Increment the total song count."""
-        cls.count += 1
+    # # Class methods
+    # @classmethod
+    # def add_song_to_count(cls):
+    #     """Increment the total song count."""
+    #     cls.count += 1
 
-    @classmethod
-    def add_to_genres(cls):
-        """Add unique genres to the genres list."""
-        # Convert genres list to a list of unique genres
-        cls.genres = list(dict.fromkeys(cls.genres))
+    # @classmethod
+    # def add_to_genres(cls):
+    #     """Add unique genres to the genres list."""
+    #     # Convert genres list to a list of unique genres
+    #     cls.genres = list(dict.fromkeys(cls.genres))
 
-    @classmethod
-    def add_to_artists(cls):
-        """Add unique artists to the artists list."""
-        # Convert artists list to a list of unique artists
-        cls.artists = list(dict.fromkeys(cls.artists))
+    # @classmethod
+    # def add_to_artists(cls):
+    #     """Add unique artists to the artists list."""
+    #     # Convert artists list to a list of unique artists
+    #     cls.artists = list(dict.fromkeys(cls.artists))
 
-    @classmethod
-    def add_to_genre_count(cls):
-        """Update the genre_count dictionary with the number of songs per genre."""
-        cls.genre_count = {}  # Reset dictionary
-        for genre in cls.genres:
-            cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
+    # @classmethod
+    # def add_to_genre_count(cls):
+    #     """Update the genre_count dictionary with the number of songs per genre."""
+    #     cls.genre_count = {}  # Reset dictionary
+    #     for genre in cls.genres:
+    #         cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
 
-    @classmethod
-    def add_to_artist_count(cls):
-        """Update the artist_count dictionary with the number of songs per artist."""
-        cls.artist_count = {}  # Reset dictionary
-        for artist in cls.artists:
-            cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
+    # @classmethod
+    # def add_to_artist_count(cls):
+    #     """Update the artist_count dictionary with the number of songs per artist."""
+    #     cls.artist_count = {}  # Reset dictionary
+    #     for artist in cls.artists:
+    #         cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
